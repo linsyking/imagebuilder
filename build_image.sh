419f4728966007ce62be7de6486e95393f25de9e
linsyking <kxiang@umich.edu>
2025-01-31 22:55:38 -0500

    fix: image build

--- build_image.sh
+++ build_image.sh
@@ -18,47 +18,47 @@ IMAGE_DIR=compile/imagebuilder-diskimage
 MOUNT_POINT=compile/image-mnt
 
 IMAGE_SIZE=1300M
-IMG=${IMAGE_DIR}/arch.img
+IMG=${IMAGE_DIR}/fedora.img
 
 truncate -s ${IMAGE_SIZE} ${IMG}
 
-FLP=$(losetup -f)
+FLP=$(sudo losetup -f)
 
-losetup ${FLP} $IMG
+sudo losetup ${FLP} $IMG
 
 # clear the partition table and reread it via partprobe
-sgdisk -Z ${FLP}
-partprobe ${FLP}
+sudo sgdisk -Z ${FLP}
+sudo partprobe ${FLP}
 
 # create a fresh partition table and reread it via partprobe
-sgdisk -C -e -G ${FLP}
-partprobe ${FLP}
+sudo sgdisk -C -e -G ${FLP}
+sudo partprobe ${FLP}
 
 # create the chomeos partition structure and reread it via partprobe
-cgpt create ${FLP}
-partprobe ${FLP}
+sudo cgpt create ${FLP}
+sudo partprobe ${FLP}
 
 # create two boot partitions and set them as bootable
 # two to have a second one to play around just in case - it just costs 32m
-cgpt add -i 1 -t kernel -b 8192 -s 65536 -l KernelA -S 1 -T 2 -P 10 ${FLP}
-cgpt add -i 2 -t kernel -b 73728 -s 65536 -l KernelB -S 0 -T 2 -P 5 ${FLP}
+sudo cgpt add -i 1 -t kernel -b 8192 -s 65536 -l KernelA -S 1 -T 2 -P 10 ${FLP}
+sudo cgpt add -i 2 -t kernel -b 73728 -s 65536 -l KernelB -S 0 -T 2 -P 5 ${FLP}
 
 sleep 1
 
-fdisk ${FLP} < gpt-partitions.txt
+sudo fdisk ${FLP} < gpt-partitions.txt
 sleep 1
-partprobe ${FLP}
+sudo partprobe ${FLP}
 sleep 1
-losetup -d ${FLP}
+sudo losetup -d ${FLP}
 sleep 1
-losetup --partscan ${FLP} $IMG
+sudo losetup --partscan ${FLP} $IMG
 
 echo "Partitioning done"
 read -p "Press enter to continue"
 
-dd if=${DOWNLOAD_DIR}/boot.dd of=${FLP}p1 status=progress
+sudo dd if=${DOWNLOAD_DIR}/boot.dd of=${FLP}p1 status=progress
 
-mkfs -t btrfs -m single -L rootpart ${FLP}p3
+sudo mkfs -t btrfs -m single -L rootpart ${FLP}p3
 sudo mount -o ssd,compress-force=zstd,noatime,nodiratime ${FLP}p3 ${MOUNT_POINT}
 
 echo "copying over the root fs to the target image - this may take a while ..."
@@ -67,4 +67,4 @@ sudo rsync -axADHSX --info=progress2 --no-inc-recursive ${BUILD_ROOT}/ ${MOUNT_P
 read -p "Done. Press enter to umount all the things."
 
 sudo umount ${MOUNT_POINT}
-losetup -d ${FLP}
+sudo losetup -d ${FLP}
--- flash_disk.sh
+++ flash_disk.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
-    exit
-fi
-
 IMAGE_DIR=compile/imagebuilder-diskimage
-IMG=${IMAGE_DIR}/arch.img
+IMG=${IMAGE_DIR}/fedora.img
 FLASH_DEV=/dev/sda
 
-dd bs=4M if=${IMG} of=${FLASH_DEV} status=progress
+sudo dd bs=4M if=${IMG} of=${FLASH_DEV} status=progress
 
 sync
 
-growpart ${FLASH_DEV} 4
-mkdir -p /mnt/roottmp
-mount ${FLASH_DEV}4 /mnt/roottmp
-btrfs filesystem resize max /mnt/roottmp
-umount /mnt/roottmp
-rmdir /mnt/roottmp
+sudo growpart ${FLASH_DEV} 3
+sudo mkdir -p /mnt/roottmp
+sudo mount ${FLASH_DEV}3 /mnt/roottmp
+sudo btrfs filesystem resize max /mnt/roottmp
+sudo umount /mnt/roottmp
+sudo rmdir /mnt/roottmp
