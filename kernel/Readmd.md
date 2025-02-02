aa293030d8d933e62952bcc4478bf6b476cc115e
linsyking <kxiang@umich.edu>
2025-02-01 21:55:26 -0500

    docs

--- kernel/Readmd.md
+++ kernel/Readmd.md
@@ -1,1 +1,16 @@
 # Build Custom Kernel
+
+Run script `build_kernel.sh <KERNEL VERSION>`. It will clone and compile the linux kernel you specified.
+
+Example:
+
+```bash
+./build_kernel.sh v6.13.1
+```
+
+
+If you are cross compiling the kernel, directly specify the compiler tool chain:
+
+```bash
+CROSS_COMPILE=aarch64-linux-gnu- ./build_kernel.sh v6.13.1
+```
--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
 # A script to build the custom kernel
+
+set -e
+
+mkdir -p compile
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git --branch $1 --single-branch --depth 1 compile/linux-$1
+mkdir -p compile/tarball
+cp .config compile/linux-$1
+cp cmdline compile/linux-$1
+cd compile/linux-$1
+
+export ARCH=arm64
+
+make olddefconfig
+
+make menuconfig
+
+echo "Kernel config ready. Start building."
+
+make -j 8 Image dtbs modules
+
+export kver=$(make kernelrelease)
+
+echo $kver
+
+find . -type f -name '*.ko' | xargs -n 1 objcopy --strip-unneeded
+
+echo "Kernel built. Installing modules."
+
+sudo make modules_install
+
+sudo make headers_install INSTALL_HDR_PATH=/usr/src/linux-$kver
